d82d2ef4e23e75f6cf0d688996f0157111479c24
Rahul Singh Raghav <officialrahulsr@gmail.com>
2021-07-11 21:15:49 +0530

    Added Search bar and Search button with styling

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -6,8 +6,11 @@ import Typography from '@material-ui/core/Typography';
 class Header extends Component{
     render(){
         return(
-            <div className = "heading">
-                <Typography variant = "h2" component = "headline">Recipe Finder</Typography>
+            <div>
+                <div className = "heading">
+                    <Typography variant = "h2" component = "headline">Recipe Finder</Typography>
+                </div>
+
             </div>
         )
     }
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,12 +1,40 @@
+import { Typography } from '@material-ui/core';
 import React, {Component} from 'react';
 import Header from '../common/header/Header';
 import "./Home.css";
 
+
+
 class Home extends Component{
+    
+    constructor()
+    {
+        super();
+        this.state = {
+            greetingDisplay : "dispBlock",
+            errorDisplay: "dispNone"
+
+        }
+    }
+
     render(){
         return (
             <div>
                 <Header/>
+                <div>
+                    <div className = "flex-container">
+                        <div className = "search">
+                            <input placeholder = "Enter the Name of the Dish" style = {{paddingTop:10, paddingBottom:10, paddingLeft:20, paddingRight:20, fontSize:22, border: 0, borderRadius: 10}}></input>
+                        </div>
+                        <div>
+                            <button className = "btn"  >Get Recipes</button>
+                        </div>
+                    </div>
+                    <Typography className = {this.state.errorDisplay} variant = "subtitle2">No Data Has Been Recieved</Typography>
+                    <Typography variant ="h4" className = {this.state.greetingDisplay} style ={{textAlign:'center', marginTop: 16, fontWeight: 'bold'}}component = "div">Type a Dish Name to Search for it's ingredient</Typography>
+                    
+                    
+                </div>
             </div>
         )
     }
