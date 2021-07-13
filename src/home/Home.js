2c0fa87e6202e5db000c897e9960d64f6326e5a3
Rahul Singh Raghav <officialrahulsr@gmail.com>
2021-07-13 14:17:32 +0530

    Added greeting and error display functionality on searching

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,6 +2,9 @@ import { Typography } from '@material-ui/core';
 import React, {Component} from 'react';
 import Header from '../common/header/Header';
 import "./Home.css";
+import RecipeCard from "../recipecards/RecipeCard";
+
+
 
 
 
@@ -12,10 +15,19 @@ class Home extends Component{
         super();
         this.state = {
             greetingDisplay : "dispBlock",
-            errorDisplay: "dispNone"
-
+            errorDisplay: "dispNone",
+            searchInput: "",
+            recipes: null,
         }
+        
     }
+
+    searchInputChangeHandler = (event) =>{
+        this.setState({searchInput: event.target.value})
+    }
+
+
+    
 
     render(){
         return (
@@ -24,15 +36,17 @@ class Home extends Component{
                 <div>
                     <div className = "flex-container">
                         <div className = "search">
-                            <input placeholder = "Enter the Name of the Dish" style = {{paddingTop:10, paddingBottom:10, paddingLeft:20, paddingRight:20, fontSize:22, border: 0, borderRadius: 10}}></input>
+                            <input placeholder = "Enter the Name of the Dish"
+                            style = {{paddingTop:10, paddingBottom:10, paddingLeft:20, paddingRight:20, fontSize:22, border: 0, borderRadius: 10}}
+                            onChange = {this.searchInputChangeHandler}></input>
                         </div>
                         <div>
-                            <button className = "btn"  >Get Recipes</button>
+                            <button className = "btn" onClick = {this.recipeSearchHandler} >Get Recipes</button>
                         </div>
                     </div>
-                    <Typography className = {this.state.errorDisplay} variant = "subtitle2">No Data Has Been Recieved</Typography>
-                    <Typography variant ="h4" className = {this.state.greetingDisplay} style ={{textAlign:'center', marginTop: 16, fontWeight: 'bold'}}component = "div">Type a Dish Name to Search for it's ingredient</Typography>
-                    
+                    <Typography className = {this.state.errorDisplay} variant = "h6" style ={{textAlign:'center', marginTop: 16}}><span>No Data Has Been Recieved</span></Typography>
+                    <Typography variant ="h4" className = {this.state.greetingDisplay} style ={{textAlign:'center', marginTop: 16, fontWeight: 'bold'}}component = "div"><span>Type a Dish Name to Search for it's ingredient</span></Typography>
+
                     
                 </div>
             </div>
